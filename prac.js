dd62ff693944c9821e62ea40055641835685a51a
TEENOLA <adeoluwa.adenola@gmail.com>
2023-07-23 20:41:17 +0100

    add

--- prac.js
+++ prac.js
@@ -1,18 +1,84 @@
 const buttons = document.querySelectorAll('button');
-const screen = document.querySelector('#screen')
+let screen = document.querySelector('#screen');
+let output = '0';
+let runningCalc = '0';
 
-let accumulativeInput
-let input = []
-function calculate(button){
-  const buttonVal = button.textContent
+function input(value){
+  if(isNaN(parseInt(value))){
+    forSymbol(value);
+  } else {
+    forNumber(value);
+  }
+  align();
+}
 
-  input.push(buttonVal)
-  accumulativeInput = input.join('')
-  screen.textContent = accumulativeInput
+function forNumber(number){
+  if (output === '0'){
+    output = number;
+  } else {
+    output += number;
+  }
+}
 
-  console.log(input)
+function mathSymbols(value){
+  if(output === '0'){
+    return;
+  }
+  
+  const integerOutput = parseInt(output)
+
+  if(runningCalc === '0'){
+    runningCalc = integerOutput
+  } else {
+    calculate(integerOutput)
+  }
+  output = '0';
+  lastSymbol = value;
+  console.log(runningCalc)
+}
+
+function calculate(integerOutput){
+  if( lastSymbol === '+'){
+    runningCalc += integerOutput
+  }
+  if( lastSymbol === '-'){
+    runningCalc -= integerOutput
+  }
+  if( lastSymbol === '*'){
+    runningCalc *= integerOutput
+  }
+  if( lastSymbol === '/'){
+    runningCalc /= integerOutput
+  }
+} 
+
+function forSymbol(symbol){
+  switch(symbol){
+    case 'C':
+      output = '0'
+      break;
+    case '←':
+      if (output.length === 1){
+        output = '0'
+      }  else {
+        output = output.substring(0, output.length - 1)
+      }
+      break;
+    case '=':
+    output = runningCalc;
+    break;
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+      mathSymbols(symbol)
+      break;
+  }
 
 }
 
+function align(){
+  screen.innerText = output;
+}
 
-buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
+buttons.forEach(button => button.addEventListener('click', (e) => input(e.target.innerText)))
